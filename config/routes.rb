4ede80a693fb72cf7fdb3f1b0edd0120a3388362
Romain Campech <romain.campech@city.ac.uk>
2018-05-14 11:04:47 +0200

    the spoon

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  # '/'
+  root to: 'pages#home'
+
+  # VERB  PATH          CONTROLLER#ACTION
+    get  '/about', to: 'pages#about'
+    get  '/contact', to: 'pages#contact'
+
+  # restaurants                                       overriding the prefix
+  get '/restaurants', to: 'restaurants#index'
+  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+  post '/restaurants', to: 'restaurants#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
